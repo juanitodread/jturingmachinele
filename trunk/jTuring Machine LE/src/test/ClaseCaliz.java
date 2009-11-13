adea2f43aada944dc54d076022c0636d53f38c6f
cosmo.marce <cosmo.marce@5534aea4-d072-11de-8f89-a3abdf1de2f0>
2009-11-13 17:27:55 +0000

    Clase de Caliz

---
+++ trunk/jTuring Machine LE/src/test/ClaseCaliz.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ *
+ * @author msena
+ */
+public class ClaseCaliz {
+
+    private static String saludo= "HOLA";
+
+    ClaseCaliz(String s){
+        saludo=s;
+    }
+
+    public static String getSaludo(){
+        return saludo;
+    }
+
+}
