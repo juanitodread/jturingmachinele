8d0e9881fefa824154036f0777c76e541d26b85a
juanitodread <juanitodread@5534aea4-d072-11de-8f89-a3abdf1de2f0>
2009-11-13 17:40:31 +0000

    otro commit

--- trunk/jTuring Machine LE/src/test/Test.java
+++ trunk/jTuring Machine LE/src/test/Test.java
@@ -11,6 +11,6 @@ package test;
  */
 public class Test {
     public static void main(String[] args){
-        System.out.println(ClaseCaliz.getSaludo());
+        System.out.println("hola");
     }
 }
