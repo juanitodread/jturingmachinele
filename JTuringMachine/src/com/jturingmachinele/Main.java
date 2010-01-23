53be674ff6189318913ca86815f9b53755302ab6
juanitodread <juanitodread@5534aea4-d072-11de-8f89-a3abdf1de2f0>
2010-01-23 01:07:08 +0000

    Clase principal

---
+++ JTuringMachine/src/com/jturingmachinele/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jturingmachinele;
+
+
+
+/**
+ *
+ * @author jsandoval
+ */
+public class Main {
+    public static void main(String[] args){
+
+   }
+}
