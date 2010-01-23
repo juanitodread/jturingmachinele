38e8f408e1cb56af16fcbde0aab15470cfeca8e4
cosmo.marce <cosmo.marce@5534aea4-d072-11de-8f89-a3abdf1de2f0>
2010-01-23 18:58:38 +0000

    Correcciones

--- JTuringMachine/src/com/jturingmachinele/graphics/transiciones/TransicionArco.java
+++ JTuringMachine/src/com/jturingmachinele/graphics/transiciones/TransicionArco.java
@@ -5,14 +5,23 @@
 
 package com.jturingmachinele.graphics.transiciones;
 
+import com.jturingmachinele.graphics.estados.Estado;
 import java.awt.Graphics;
 import java.io.Serializable;
+import java.util.Observable;
 
 public class TransicionArco extends Transicion implements Serializable {
+
+    public TransicionArco(Estado nodoInicial, Estado nodoFinal, String etiqueta){
+        super(nodoInicial, nodoFinal, etiqueta);
+    }
 
     @Override
     public void dibujar(Graphics g) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
