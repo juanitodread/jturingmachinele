acd49d413c0c2d4229b59280f748644cf05dbd49
juanitodread <juanitodread@5534aea4-d072-11de-8f89-a3abdf1de2f0>
2010-02-04 23:55:35 +0000

    Correcciones.

--- trunk/JTuringMachine/src/com/jturingmachinele/Main.java
+++ trunk/JTuringMachine/src/com/jturingmachinele/Main.java
@@ -1,18 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * @(#)Main.java	1.0 04/02/2010
+ *
+ * TODOS LOS DERECHOS RESERVADOS PARA LOS DESARROLLADORES DEL PROYECTO jTuringMachine.
  */
 
 package com.jturingmachinele;
 
-
+import com.jturingmachinele.visual.Aplicacion;
 
 /**
+ * Clase principal de la aplicación.
  *
- * @author jsandoval
+ * @author Juan Sandoval
+ * @version 1.0
  */
 public class Main {
     public static void main(String[] args){
-
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Aplicacion().setVisible(true);
+            }
+        });
    }
 }
