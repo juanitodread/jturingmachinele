6632909ad4fbf8603aef37760a36c818bd85c556
juanitodread@gmail.com <juanitodread@gmail.com@5534aea4-d072-11de-8f89-a3abdf1de2f0>
2011-07-10 00:56:07 +0000

    Se añaden los terminos de la licencia GPL 3 a cada archivo de código.

--- trunk/JTuringMachine/src/com/jturingmachinele/visual/Splash.java
+++ trunk/JTuringMachine/src/com/jturingmachinele/visual/Splash.java
@@ -1,9 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
+ *  TODOS LOS DERECHOS RESERVADOS PARA LOS DESARROLLADORES DEL PROYECTO jTuringMachine.
+ *   
+ *  Copyright (C) 2009  SystarSystems.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or 
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ * 
  * Splash.java
  *
  * Created on 6/02/2010, 01:29:23 PM
@@ -13,7 +25,6 @@ package com.jturingmachinele.visual;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFrame;
 
 /**
  *
